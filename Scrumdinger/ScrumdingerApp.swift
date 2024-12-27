52526bdec5d0467e2cb734b41e4a58da986d5729
Rishabh Saxena <orishabh1@gmail.com>
2024-12-27 20:09:14 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rishabh Saxena on 27.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
